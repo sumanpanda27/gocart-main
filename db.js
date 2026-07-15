1bdc9ec81aa2eb255fc9c10f77853427d126bb72
SUMAN PANDA <sumanpanda27@gmail.com>
2026-07-15 15:31:01 +0530

    fixed at all

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
 import 'dotenv/config'
 
 import { PrismaClient } from './lib/generated/prisma/client';
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaNeon } from '@prisma/adapter-neon'
 
 
-const adapter = new PrismaPg({
+const adapter = new PrismaNeon({
     connectionString: process.env.DATABASE_URL,
-});
-
-export const prisma = new PrismaClient({ adapter });
+})
+export const prisma = new PrismaClient({ adapter })
