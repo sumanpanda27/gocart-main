d4948e4e88f645cd8fbb9267e4f90d940143cbba
SUMAN PANDA <sumanpanda27@gmail.com>
2026-07-15 13:41:02 +0530

    fix the error of ts

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ import { PrismaNeon } from '@prisma/adapter-neon'
 // import { PrismaClient } from '@prisma/client'
 
 const adapter = new PrismaNeon({
-    connectionString: process.env.DATABASE_URL!,
+    connectionString: process.env.DATABASE_URL,
 })
 
 export const prisma = new PrismaClient({ adapter })
