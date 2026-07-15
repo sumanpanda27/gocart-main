0bfa86938c6f7eb1909d2c62705d52f428c60b5b
SUMAN PANDA <sumanpanda27@gmail.com>
2026-07-15 15:15:14 +0530

    fixed issues

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 import 'dotenv/config'
-// import { PrismaClient } from "./path/to/generated/prisma";
+
 import { PrismaClient } from './lib/generated/prisma/client';
-// import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 
 const adapter = new PrismaPg({
